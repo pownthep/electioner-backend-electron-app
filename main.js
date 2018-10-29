1504fa5b48b370332d13f20c5e422feda0bca82e
Pownthep <pownthepofficial@gmail.com>
2018-10-29 17:48:52 +0700

    Initial commit

---
+++ main.js
@@ -0,0 +1,43 @@
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
+const url = require("url");
+
+let win;
+
+function createWindow() {
+  win = new BrowserWindow({ width: 800, height: 600 });
+
+  // load the dist folder from Angular
+  win.loadURL(`C:/Users/Pownthep Laokhunthot/Desktop/Project/admin/dist/admin/index.html`);
+ 
+  // The following is optional and will open the DevTools:
+  // win.webContents.openDevTools()
+
+  win.on("closed", () => {
+    win = null;
+  });
+}
+
+app.on("ready", createWindow);
+
+// on macOS, closing the window doesn't quit the app
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
+
+// initialize the app's main window
+app.on("activate", () => {
+  if (win === null) {
+    createWindow();
+  }
+});
+
+  // SSL/TSL: this is the self signed certificate support
+app.on('certificate-error', (event, webContents, url, error, certificate, callback) => {
+    // On certificate error we disable default behaviour (stop loading the page)
+    // and we then say "it is all fine - true" to the callback
+    event.preventDefault();
+    callback(true);
+  });
